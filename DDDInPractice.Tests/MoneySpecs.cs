aa6bfa9a1c3a72f40a3cd97c177e801079da56e3
Cuneyt B <cuneytbozyel@gmail.com>
2019-01-17 23:03:22 +0300

    commit message

--- DDDInPractice.Tests/MoneySpecs.cs
+++ DDDInPractice.Tests/MoneySpecs.cs
@@ -1,20 +1,25 @@
 ﻿using DDDInPractice.Logic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
 
 namespace DDDInPractice.Tests
 {
     public class MoneySpecs
     {
+        [Fact]
         public void Sum_Of_two_Produces_Correct_Result()
         {
             Money money1 = new Money(1, 2, 3, 4, 5, 6);
             Money money2 = new Money(1, 2, 3, 4, 5, 6);
 
             Money sum = money1 + money2;
+            sum.OneCentCount.Should().Be(2);
+            sum.TenCentCount.Should().Be(4);
+            sum.QuarterCount.Should().Be(6);
+            sum.OneDollarCount.Should().Be(8);
+            sum.FiveDollarCount.Should().Be(10);
+            sum.TwentyDollarCount.Should().Be(12);
+
         }
     }
 }
